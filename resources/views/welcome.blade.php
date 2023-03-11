5f1895606194b1b497d6da8d57c51b01aaf5414e
Gabriele Scogna <gabrysco@live.it>
2023-03-11 11:19:15 +0100

    Quinto-Sesto-Progetto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <div class="container-fluid p-5 bg-warning text-white text-center">
+        <div class="row justify-content-center"></div>
+        <div class="col-12">
+            <h1 class="display-1">Benvenuto</h1>
+        </div>
+    </div>
+</x-lauout>
