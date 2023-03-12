cf5b72eaa12c5e78017e66f026f73a55e8bed21e
Gabriele Scogna <gabrysco@live.it>
2023-03-12 16:18:28 +0100

    form per moidifica fatto

--- resources/views/manga/edit.blade.php
+++ resources/views/manga/edit.blade.php
@@ -23,17 +23,21 @@
                     
                     <div class="mb-3">
                         <label for="cover" class="form-label">Cover della serie</label>
-                        <input type="file" name="cover" class="form-control" id="cover">
+                        <input type="file" name="cover" class="form-control" id="cover" >
+                    </div>
+                    <div class="mt-3">
+                        <label for="existingCover" class="form-label">Cover attuale</label>
+                        <img style="width: 300px "src="{{Storage::url($manga->cover)}}" alt="">
                     </div>
                     
                     <div class="mb-3">
                         <label for="name" class="form-label">Nome della serie</label>
-                        <input type="text" name="name" class="form-control" id="name">
+                        <input type="text" name="name" class="form-control" id="name" value="{{$manga->name}}">
                     </div>
                     
                     <div class="mb-3">
                         <label for="publisher" class="form-label">Nome della casa di stampa</label>
-                        <input type="text" name="publisher" class="form-control" id="publisher">
+                        <input type="text" name="publisher" class="form-control" id="publisher" value="{{$manga->publisher}}">
                     </div>
                     
                     <button type="submit" class="btn btn-primary">Aggiungi Modifica</button>
